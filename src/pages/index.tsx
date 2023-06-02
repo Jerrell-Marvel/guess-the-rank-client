5f5f62f5e901d828d3b64265d71d97c7e7a5f14b
Jerrell Marvel <jerrell.marvel88@gmail.com>
2023-06-02 19:46:53 +0700

    create home page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,14 +2,24 @@ import Image from "next/image";
 import { Inter } from "next/font/google";
 import axios from "axios";
 import { useState, useEffect } from "react";
+import { GetStaticProps, InferGetStaticPropsType } from "next";
+import { Categories } from "@/types/category";
 
 const inter = Inter({ subsets: ["latin"] });
 
-export default function Home() {
-  return <div>TESTING</div>;
+export default function Home({ categories }: InferGetStaticPropsType<typeof getStaticProps>) {
+  return (
+    <div>
+      {categories.map((category) => {
+        return <div key={category._id}>{category.name}</div>;
+      })}
+    </div>
+  );
 }
 
-// export const getStaticProps = async () => {
-//   const response = await axios.get("http://localhost:5000/api/v1/clip/6479728f88e02872c413728d");
-//   return { props: { hi: response.data.clip._id } };
-// };
+export const getStaticProps: GetStaticProps<{ categories: Categories }> = async () => {
+  const response = await axios.get<Categories>("http://localhost:5000/api/v1/categories");
+  const data = response.data;
+
+  return { props: { categories: data } };
+};
---
+++ src/types/category.d.ts
@@ -0,0 +1,8 @@
+export type Category = {
+  _id: string;
+  name: string;
+  description: string;
+  ranks: string[];
+};
+
+export type Categories = Category[];
