5bc7db291623d14f7cfdd698773169b57c7cf8bf
Jerrell Marvel <jerrell.marvel88@gmail.com>
2023-06-04 20:58:59 +0700

    create clips filter by status

--- src/pages/admin/[category].tsx
+++ src/pages/admin/[category].tsx
@@ -1,19 +1,21 @@
 import { useRouter } from "next/router";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { useQuery, useQueryClient } from "react-query";
 import axios from "axios";
 import { Clips } from "@/types/clip";
+const clipStatusArr = ["pending", "verified"];
 
 const AdminCategoryPage = () => {
   const router = useRouter();
   const queryClient = useQueryClient();
+  const [clipStatus, setClipStatus] = useState("pending");
 
   const { data: clips } = useQuery<Clips>({
-    queryKey: ["category", router.query.category],
+    queryKey: ["category", router.query.category, clipStatus],
     queryFn: async () => {
       const response = await axios.get<Clips>(`http://localhost:5000/api/v1/clips/${router.query.category}`, {
         params: {
-          status: "pending",
+          status: clipStatus,
         },
         withCredentials: true,
       });
@@ -30,16 +32,31 @@ const AdminCategoryPage = () => {
   //     queryClient.refetchQueries(["test"]);
   //   }
   // }, [router.isReady, queryClient]);
+
   return (
     <div>
+      <div>
+        {clipStatusArr.map((stat) => {
+          return (
+            <span
+              className={`${clipStatus === stat ? "text-xl" : ""}`}
+              key={stat}
+              onClick={() => setClipStatus(stat)}
+            >
+              {stat}
+            </span>
+          );
+        })}
+      </div>
       <div>{router.query.category}</div>
       <div>
         {clips?.map((clip) => {
           return (
             <div key={clip._id}>
-              <span>{clip.link}</span>
-              <span>{clip.status}</span>
-              <button>Verify</button>
+              <span className="text-blue-600 underline">{clip.link} </span>
+              <span>{clip.status} </span>
+
+              {clip.status === "pending" ? <button>Verify </button> : null}
             </div>
           );
         })}
