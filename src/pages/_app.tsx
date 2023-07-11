67850b4aeba5490d897ad9d3f4ec88a60dad3716
Jerrell Marvel <jerrell.marvel88@gmail.com>
2023-07-11 20:34:59 +0700

    insall and setup react-toastify

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -5,10 +5,25 @@ import { useState } from "react";
 import { QueryClient, QueryClientProvider } from "react-query";
 import { ReactQueryDevtools } from "react-query/devtools";
 
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+
 export default function App({ Component, pageProps }: AppProps) {
   const [queryClient] = useState(() => new QueryClient());
   return (
     <div className="flex justify-center bg-slate-950">
+      <ToastContainer
+        position="top-center"
+        autoClose={3000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+        theme="dark"
+      />
       <QueryClientProvider client={queryClient}>
         <Navbar />
         <div className="max-w-7xl w-full">
